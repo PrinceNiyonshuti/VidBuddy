1f63615030b64e382e3369f0adbd68215a64d169
PrinceNiyonshuti <npprince47@gmail.com>
2022-10-24 23:34:01 +0200

    (screens): Implemented Contorls for video call

--- App.js
+++ App.js
@@ -76,8 +76,60 @@ function JoinScreen(props) {
   );
 }
 
-function ControlsContainer() {
-  return null;
+const Button = ({onPress, buttonText, backgroundColor}) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{
+        backgroundColor: backgroundColor,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 12,
+        borderRadius: 4,
+      }}>
+      <Text style={{color: 'white', fontSize: 12}}>{buttonText}</Text>
+    </TouchableOpacity>
+  );
+};
+
+function ControlsContainer({join, leave, toggleWebcam, toggleMic}) {
+  return (
+    <View
+      style={{
+        padding: 24,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}>
+      <Button
+        onPress={() => {
+          join();
+        }}
+        buttonText={'Join'}
+        backgroundColor={'#1178F8'}
+      />
+      <Button
+        onPress={() => {
+          toggleWebcam();
+        }}
+        buttonText={'Toggle Webcam'}
+        backgroundColor={'#1178F8'}
+      />
+      <Button
+        onPress={() => {
+          toggleMic();
+        }}
+        buttonText={'Toggle Mic'}
+        backgroundColor={'#1178F8'}
+      />
+      <Button
+        onPress={() => {
+          leave();
+        }}
+        buttonText={'Leave'}
+        backgroundColor={'#FF0000'}
+      />
+    </View>
+  );
 }
 
 function MeetingView() {
